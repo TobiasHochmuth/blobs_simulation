6297a331c1bd75050e52b84279d006bad4c5ee00
ffavale <francesco@favale.eu>
2021-05-28 19:03:21 +0200

    Diffusion works. ~2.6a/2.6b

--- Main.java
+++ Main.java
@@ -8,19 +8,123 @@
         
         //Initialize simuation enviornment
 
+        //Import setup from setup.json
+        final int xSize = 720;
+        final int ySize = 720;
+        final int[] canvas_res = {xSize, ySize};
+        final int[] max_a = {3, 3};
+        final int thickCount = 10;
+        final int thinCount = 10;
+        final int thicR = 10;
+        final int thinR = 5;
+
+
+        //Start window
+        Canvas simulation_canvas = new Canvas(xSize, ySize, 75, 0);
+        //Start pen
+        Pen pen = new Pen(simulation_canvas);
+
+
+        //Initialize all the blobs
+        //2 different arrays for diffent types of blobs; 1 large major array.
+        blob[] thickBlob_array = new blob[thickCount];
+        blob[] thinBlob_array = new blob[thickCount];
+        blob[] generalBlob_array = new blob[(thickCount+thinCount)];
+
+        //generate the blob arrays and polulate them respectivley
+        //thick
+        for (int i = 0; i < thickCount; i++)
+        {
+            blob blob = new blob(i, thinCount, true); //new blob
+            blob.masterID = i+1; //assign id
+            blob.radius = thicR; //determine radius
+            thickBlob_array[i] = blob; //put in thickarray
+
+            //assign random starting positions to every blob
+            blob.posX = blob.randomPosition(canvas_res)[0];
+            blob.posY = blob.randomPosition(canvas_res)[1];
+            //assign random starting speeds to every blob
+            blob.dX = blob.randomSpeed(max_a)[0];
+            blob.dY = blob.randomSpeed(max_a)[1];
+
+            generalBlob_array[i] = blob; //put in general array
+        }
+        //thin
+        for (int i = 0; i < thinCount; i++)
+        {
+            blob blob = new blob(i, thickCount, true); //new blob
+            blob.masterID = -i-1; //assign id
+            blob.radius = thinR; //determine radius
+            thinBlob_array[i] = blob; //put in thinarray
+
+            //assign random starting positions to every blob
+            blob.posX = blob.randomPosition(canvas_res)[0];
+            blob.posY = blob.randomPosition(canvas_res)[1];
+            //assign random starting speeds to every blob
+            blob.dX = blob.randomSpeed(max_a)[0];
+            blob.dY = blob.randomSpeed(max_a)[1];
+            
+            generalBlob_array[i+thickCount-1] = blob; //put in general array
+        }
+
+
         
         //Main simulation loop
-        for (long i = 0; true; i++)
+        for (int i = 0; true; i++)
         {
-            
-            //testcode
-            System.out.printf("Iteration %s%n", i);
-            //Stop simulation after n tries; debug
-            if (i == 100)
+            //Clear previous frame
+            simulation_canvas.clear();
+
+            //Generate new speeds
+            //thick
+            for (int j = 0; j < (thickBlob_array.length); j++)
             {
-                return;
+                thickBlob_array[j].dX = thickBlob_array[j].randomSpeed(max_a)[0];
+                thickBlob_array[j].dY = thickBlob_array[j].randomSpeed(max_a)[1];
+            }
+            //thin
+            for (int j = 0; j < (thinBlob_array.length); j++)
+            {
+                thinBlob_array[j].dX = thinBlob_array[j].randomSpeed(max_a)[0];
+                thinBlob_array[j].dY = thinBlob_array[j].randomSpeed(max_a)[1];
             }
 
+
+            //Update positions
+            //thick
+            for (int j = 0; j < (thickBlob_array.length); j++)
+            {
+                thickBlob_array[j].posX = thickBlob_array[j].posX + thickBlob_array[j].dX;
+                thickBlob_array[j].posY = thickBlob_array[j].posY + thickBlob_array[j].dY;
+            }
+            //thin
+            for (int j = 0; j < (thinBlob_array.length); j++)
+            {
+                thinBlob_array[j].posX = thinBlob_array[j].posX + thinBlob_array[j].dX;
+                thinBlob_array[j].posY = thinBlob_array[j].posY + thinBlob_array[j].dY;
+            }
+
+
+            //Iteration counter
+            System.out.printf("Iteration %s%n", i);
+
+            //Draw simulation
+            for (int j = 0; j < thickCount; j++)
+            {
+                pen.drawCircle(thickBlob_array[j].posX, thickBlob_array[j].posY, thickBlob_array[j].radius, Color.RED, true);
+            }
+            for (int j = 0; j < thinCount; j++)
+            {
+                pen.drawCircle(thinBlob_array[j].posX, thinBlob_array[j].posY, thinBlob_array[j].radius, Color.BLUE, true);
+            }
+
+            //Update simulation canvas after every loop
+            simulation_canvas.update();
+
+            try {
+                Thread.sleep(10);
+            } catch (Exception InterrupException) {
+            }
         }
     }
   }
--- blob.java
+++ blob.java
@@ -1,3 +1,66 @@
+import java.util.Random;
+
 public class blob {
-        
+   
+    public enum status{
+        NORMAL,
+        TOUCHING,
+        TOUCHED
+    }
+
+    int partnerCount;
+    //position
+    int posX;
+    int posY;
+    //velocity
+    int dX;
+    int dY;
+    //blob dimension and type
+    int radius;
+    boolean type; //true = thick; false = skinny;
+    //blob masterID
+    int masterID;
+    //Status
+    status blobStatus = status.NORMAL;
+
+ 
+
+    public blob(int _i, int _partnerCount, boolean _type) //init blob
+    {
+        partnerCount = _partnerCount;
+        if (_type)
+        {
+            radius = 30;
+        }
+        else {
+            radius = 12;
+        }
+    }
+    
+    double[] distance_array = new double[partnerCount];
+
+    //get ranom positions
+    public int[] randomPosition(int[] _canvas_res)
+    {
+        Random rand = new Random(); //random number genertor
+        int[] position = new int[2]; //init position vector
+
+        position[0] = rand.nextInt(_canvas_res[0] + 1); //asign x position
+        position[1] = rand.nextInt(_canvas_res[1] + 1); // asign y position
+
+        return position; //return
+    }
+
+    //get random set of speeds
+    public int[] randomSpeed(int[] _max_a)
+    {
+        Random rand = new Random(); //random number genertor
+        int[] speed = new int[2]; //init speed vector
+
+        speed[0] = rand.nextInt(_max_a[0]*2+1) - _max_a[0]; //asign x speed
+        speed[1] = rand.nextInt(_max_a[1]*2+1) - _max_a[1]; // asign y speed
+
+        return speed; //return
+    }
+
 }
